7f478da040f466b90b050ad88fee1dfcc4828a60
unknown <alexmoura658@gmail.com>
2025-04-14 14:46:45 -0300

    Configuring routes

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { KeycloakProvider } from './auth/KeycloakProvider';
-
-export default function App() {
-  return (
-    <KeycloakProvider>
-      <div>Hello, World!</div>
-    </KeycloakProvider>
-  );
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { KeycloakProvider } from './auth/KeycloakProvider';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AppRoute } from './routes/AppRoute';
+import Home from './pages/Home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KeycloakProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={AppRoute.Home} />} />
+          <Route path={AppRoute.Home} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </KeycloakProvider>
   </StrictMode>
 );
